8e67de4bcfc77da5b96912f93fe30e25b4fe0879
Cláudio Brito <cabf.brito@gmail.com>
2023-07-27 11:31:33 -0300

    Create bank_account_system_simulation.py

---
+++ bank_account_system_simulation.py
@@ -0,0 +1,8 @@
+# Simulation of a bank system back-end:
+
+class Account():
+    def __init__(self, number, holder_name, balance, limit):
+        self.number = number
+        self.holder_name = holder_name
+        self.balance = balance
+        self.limit = limit
